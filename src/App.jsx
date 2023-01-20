f6ac67b09c8e3813cfcd70a6289d493c4ab94958
Oleksii Verbytskyi <alverba88@gmail.com>
2023-01-20 18:25:00 +0200

    Added logo component

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { Logo } from './components/Logo';
+
 export const App = () => {
   return (
     <div
@@ -10,7 +12,7 @@ export const App = () => {
         color: '#010101',
       }}
     >
-      React homework template
+      <Logo />
     </div>
   );
 };
--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,5 +1,13 @@
 import styles from './Logo.module.scss';
+import icon from 'images/icons.svg';
 
 export const Logo = () => {
-  return <>Logo</>;
+  return (
+    <a href="/" className={styles.logo}>
+      <svg className={styles.logo__icon} width={39} height={35}>
+        <use href={`${icon}#logo`}></use>
+      </svg>
+      Finance <span>Ledger</span>
+    </a>
+  );
 };
