5025165c0d19a2bed3f08e76fe51ffbf66594c18
Oleksii Verbytskyi <alverba88@gmail.com>
2023-01-20 19:57:24 +0200

    added navBar comp.

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,5 +1,35 @@
 import styles from './Navigation.module.scss';
 
 export const Navigation = () => {
-  return <>navigation</>;
+  return (
+    <nav className={styles.nav}>
+      <ul className={styles.nav__list}>
+        <li className={styles.nav__item}>
+          <a className={styles.nav__link} href="#home">
+            Home
+          </a>
+        </li>
+        <li className={styles.nav__item}>
+          <a className={styles.nav__link} href="#about">
+            About
+          </a>
+        </li>
+        <li className={styles.nav__item}>
+          <a className={styles.nav__link} href="#cases">
+            Cases
+          </a>
+        </li>
+        <li className={styles.nav__item}>
+          <a className={styles.nav__link} href="#blog">
+            Blog
+          </a>
+        </li>
+        <li className={styles.nav__item}>
+          <a className={styles.nav__link} href="#contact">
+            Contact
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
 };
