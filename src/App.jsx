df16d7c8d8fe62f33002f4e580cfe685177a353f
Oleksii Verbytskyi <alverba88@gmail.com>
2023-01-22 22:01:13 +0200

    mod hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
 import { Header } from './components/Header';
+import { Hero } from './views/Hero';
 
 export const App = () => {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <Hero />
+    </>
+  );
 };
--- src/views/Hero/Hero.jsx
+++ src/views/Hero/Hero.jsx
@@ -1,5 +1,28 @@
 import styles from './Hero.module.scss';
+import icon from 'images/icons.svg';
+import { Container } from '../../components/Container';
+import { SubTitle } from '../../components/UI/SubTitle';
+import { CommonButton } from '../../components/UI/Button';
+import { isMobile, isTablet } from '../../utils/mediaQuery';
+import { useMediaQuery } from 'react-responsive';
 
 export const Hero = () => {
-  return <>Hero</>;
+  const Mobile = isMobile(useMediaQuery);
+  const Tablet = isTablet(useMediaQuery);
+  return (
+    <section className={styles.hero}>
+      <Container className={styles.hero__container}>
+        <h1 className={styles.mainTitle}>The Sky Is The Limit</h1>
+        <SubTitle className={styles.hero__subTitle}>
+          We provide world class financial assistance
+        </SubTitle>
+        <CommonButton className={styles.hero__btn}>
+          <svg className={styles.hero__icon} width={9} height={15}>
+            <use href={`${icon}#arrow-right`}></use>
+          </svg>
+          Read More
+        </CommonButton>
+      </Container>
+    </section>
+  );
 };
