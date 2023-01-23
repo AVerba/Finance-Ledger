4e99a821eb251f8fb488c705353e8cbca5296031
Oleksii Verbytskyi <alverba88@gmail.com>
2023-01-23 16:41:29 +0200

    finished about

--- src/views/About/About.jsx
+++ src/views/About/About.jsx
@@ -7,32 +7,7 @@ export const About = () => {
   return (
     <section className={styles.about}>
       <Container className={styles.about__container}>
-        <div className={styles.about__img}>
-          {/*<picture>*/}
-          {/*  <source srcSet='../../images/about/desktop/people.webp 1x,*/}
-          {/*                  ../../images/about/desktop/people@2x.webp 2x'*/}
-          {/*          type='image/webp'*/}
-          {/*          media='(min-width: 1360px)'/>*/}
-          {/*  <source srcSet='../../images/about/tablet/people.webp 1x,*/}
-          {/*                 ../../images/about/tablet/people@2x.webp 2x'*/}
-          {/*          type='image/webp'*/}
-          {/*          media='(min-width: 768px)'/>*/}
-          {/*  <source srcSet='../../images/about/mobile/people.webp 1x,*/}
-          {/*                  ../../images/about/mobile/people@2x.webp 2x'*/}
-          {/*          type='image/webp'*/}
-          {/*          media='(max-width: 767px)'/>*/}
-          {/*  <source srcSet='../../images/about/desktop/people.jpg 1x,*/}
-          {/*                  ../../images/about/desktop/people@2x.jpg 2x'*/}
-          {/*          media='(min-width: 1360px)'/>*/}
-          {/*  <source srcSet='../../images/about/tablet/people.jpg 1x,*/}
-          {/*                 ../../images/about/tablet/people@2x.jpg 2x'*/}
-          {/*          media='(min-width: 768px)'/>*/}
-          {/*  <source srcSet='../../images/about/mobile/people.jpg 1x,*/}
-          {/*                  ../../images/about/mobile/people@2x.jpg 2x'*/}
-          {/*          media='(max-width: 767px)'/>*/}
-          {/*    <img className={styles._img} src='../../images/about/desktop/people.jpg' alt='about'/>*/}
-          {/*</picture>*/}
-        </div>
+        <div className={styles.about__img}></div>
 
         <div className={styles.about__content}>
           <p>What you are looking for </p>
