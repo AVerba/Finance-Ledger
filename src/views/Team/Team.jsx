7e713a3f758ee83528b86e9fa7690f6cfdb84c95
Oleksii Verbytskyi <alverba88@gmail.com>
2023-01-25 20:04:05 +0200

    added cards team

--- src/views/Team/Team.jsx
+++ src/views/Team/Team.jsx
@@ -1,8 +1,11 @@
 import styles from './Team.module.scss';
 import { Container } from '../../components/Container';
 import { SubTitle } from '../../components/UI/SubTitle';
+import icon from 'images/icons.svg';
+import { images } from './images';
 
 export const Team = () => {
+  const { firstImages, secondImages, thirdImages } = images;
   return (
     <section className={styles.team}>
       <Container className={styles.team__container}>
@@ -15,7 +18,326 @@ export const Team = () => {
           sapiente!
         </p>
 
-        <ul className={styles.team__list}></ul>
+        <ul className={styles.team__list}>
+          <li className={styles.itemCard}>
+            <div className={styles.overlay}>
+              <picture>
+                <source
+                  type="image/webp"
+                  media="(min-width: 1360px)"
+                  srcSet={`${firstImages.deskWebp1} 1x, ${firstImages.deskWebp2x1} 2x`}
+                />
+                <source
+                  type="image/webp"
+                  media="(min-width: 768px)"
+                  srcSet={`${firstImages.tabWebp1} 1x, ${firstImages.tabWebp2x1} 2x`}
+                />
+                <source
+                  type="image/webp"
+                  media="(max-width: 767px)"
+                  srcSet={`${firstImages.mobWebp1} 1x, ${firstImages.mobWebp2x1} 2x`}
+                />
+                <source
+                  media="(min-width: 1360px)"
+                  srcSet={`${firstImages.deskJpeg1} 1x, ${firstImages.deskJpeg2x1} 2x`}
+                />
+                <source
+                  media="(min-width: 768px)"
+                  srcSet={`${firstImages.tabJpeg1} 1x, ${firstImages.tabJpeg2x1} 2x`}
+                />
+                <source
+                  media="(max-width: 767px)"
+                  srcSet={`${firstImages.mobJpeg1} 1x, ${firstImages.mobJpeg2x1} 2x`}
+                />
+                <img
+                  className={styles._img}
+                  src={firstImages.mobJpeg1}
+                  alt="about"
+                  width="680"
+                />
+              </picture>
+              {/*<div className={styles.itemCardOverlay}>*/}
+              {/*  <ul className={styles.social__list}>*/}
+              {/*    <li className={styles.social__item}>*/}
+              {/*      <a*/}
+              {/*        href="https://www.facebook.com/"*/}
+              {/*        target="_blank"*/}
+              {/*        rel="noopener noreferrer"*/}
+              {/*      >*/}
+              {/*        <svg*/}
+              {/*          className={styles.social__item_icon}*/}
+              {/*          width={35}*/}
+              {/*          height={35}*/}
+              {/*        >*/}
+              {/*          <use href={`${icon}#facebook`}></use>*/}
+              {/*        </svg>*/}
+              {/*      </a>*/}
+              {/*    </li>*/}
+              {/*    <li className={styles.social__item}>*/}
+              {/*      <a*/}
+              {/*        href="https://twitter.com/"*/}
+              {/*        target="_blank"*/}
+              {/*        rel="noopener noreferrer"*/}
+              {/*      >*/}
+              {/*        <svg*/}
+              {/*          className={styles.social__item_icon}*/}
+              {/*          width={35}*/}
+              {/*          height={33}*/}
+              {/*        >*/}
+              {/*          <use href={`${icon}#twitter`}></use>*/}
+              {/*        </svg>*/}
+              {/*      </a>*/}
+              {/*    </li>*/}
+              {/*    <li className={styles.social__item}>*/}
+              {/*      <a*/}
+              {/*        href="https://www.youtube.com/"*/}
+              {/*        target="_blank"*/}
+              {/*        rel="noopener noreferrer"*/}
+              {/*      >*/}
+              {/*        <svg*/}
+              {/*          className={styles.social__item_icon}*/}
+              {/*          width={40}*/}
+              {/*          height={31}*/}
+              {/*        >*/}
+              {/*          <use href={`${icon}#youtube`}></use>*/}
+              {/*        </svg>*/}
+              {/*      </a>*/}
+              {/*    </li>*/}
+              {/*    <li className={styles.social__item}>*/}
+              {/*      <a*/}
+              {/*        href="https://www.linkedin.com/"*/}
+              {/*        target="_blank"*/}
+              {/*        rel="noopener noreferrer"*/}
+              {/*      >*/}
+              {/*        <svg*/}
+              {/*          className={styles.social__item_icon}*/}
+              {/*          style={{ width: "35px", height: "39px" }}*/}
+              {/*        >*/}
+              {/*          <use href={`${icon}#linkedin`}></use>*/}
+              {/*        </svg>*/}
+              {/*      </a>*/}
+              {/*    </li>*/}
+              {/*  </ul>*/}
+              {/*</div>*/}
+            </div>
+            <div className={styles.itemCard__content}>
+              <h4 className={styles.itemCard__name}>John Doe</h4>
+              <p className={styles.itemCard__pos}>President</p>
+            </div>
+          </li>
+          <li className={styles.itemCard}>
+            <div className={styles.overlay}>
+              <picture>
+                <source
+                  type="image/webp"
+                  media="(min-width: 1360px)"
+                  srcSet={`${secondImages.deskWebp2} 1x, ${secondImages.deskWebp2x2} 2x`}
+                />
+                <source
+                  type="image/webp"
+                  media="(min-width: 768px)"
+                  srcSet={`${secondImages.tabWebp2} 1x, ${secondImages.tabWebp2x2} 2x`}
+                />
+                <source
+                  type="image/webp"
+                  media="(max-width: 767px)"
+                  srcSet={`${secondImages.mobWebp2} 1x, ${secondImages.mobWebp2x2} 2x`}
+                />
+                <source
+                  media="(min-width: 1360px)"
+                  srcSet={`${secondImages.deskJpeg2} 1x, ${secondImages.deskJpeg2x2} 2x`}
+                />
+                <source
+                  media="(min-width: 768px)"
+                  srcSet={`${secondImages.tabJpeg2} 1x, ${secondImages.tabJpeg2x2} 2x`}
+                />
+                <source
+                  media="(max-width: 767px)"
+                  srcSet={`${secondImages.mobJpeg2} 1x, ${secondImages.mobJpeg2x2} 2x`}
+                />
+                <img
+                  className={styles._img}
+                  src={secondImages.mobJpeg2}
+                  alt="about"
+                  width="680"
+                />
+              </picture>
+              {/*<div className={styles.itemCardOverlay}>*/}
+              {/*  <ul className={styles.social__list}>*/}
+              {/*    <li className={styles.social__item}>*/}
+              {/*      <a*/}
+              {/*        href="https://www.facebook.com/"*/}
+              {/*        target="_blank"*/}
+              {/*        rel="noopener noreferrer"*/}
+              {/*      >*/}
+              {/*        <svg*/}
+              {/*          className={styles.social__item_icon}*/}
+              {/*          width={35}*/}
+              {/*          height={35}*/}
+              {/*        >*/}
+              {/*          <use href={`${icon}#facebook`}></use>*/}
+              {/*        </svg>*/}
+              {/*      </a>*/}
+              {/*    </li>*/}
+              {/*    <li className={styles.social__item}>*/}
+              {/*      <a*/}
+              {/*        href="https://twitter.com/"*/}
+              {/*        target="_blank"*/}
+              {/*        rel="noopener noreferrer"*/}
+              {/*      >*/}
+              {/*        <svg*/}
+              {/*          className={styles.social__item_icon}*/}
+              {/*          width={35}*/}
+              {/*          height={33}*/}
+              {/*        >*/}
+              {/*          <use href={`${icon}#twitter`}></use>*/}
+              {/*        </svg>*/}
+              {/*      </a>*/}
+              {/*    </li>*/}
+              {/*    <li className={styles.social__item}>*/}
+              {/*      <a*/}
+              {/*        href="https://www.youtube.com/"*/}
+              {/*        target="_blank"*/}
+              {/*        rel="noopener noreferrer"*/}
+              {/*      >*/}
+              {/*        <svg*/}
+              {/*          className={styles.social__item_icon}*/}
+              {/*          width={40}*/}
+              {/*          height={31}*/}
+              {/*        >*/}
+              {/*          <use href={`${icon}#youtube`}></use>*/}
+              {/*        </svg>*/}
+              {/*      </a>*/}
+              {/*    </li>*/}
+              {/*    <li className={styles.social__item}>*/}
+              {/*      <a*/}
+              {/*        href="https://www.linkedin.com/"*/}
+              {/*        target="_blank"*/}
+              {/*        rel="noopener noreferrer"*/}
+              {/*      >*/}
+              {/*        <svg*/}
+              {/*          className={styles.social__item_icon}*/}
+              {/*          style={{ width: "35px", height: "39px" }}*/}
+              {/*        >*/}
+              {/*          <use href={`${icon}#linkedin`}></use>*/}
+              {/*        </svg>*/}
+              {/*      </a>*/}
+              {/*    </li>*/}
+              {/*  </ul>*/}
+              {/*</div>*/}
+            </div>
+            <div className={styles.itemCard__content}>
+              <h4 className={styles.itemCard__name}>Jane Doe</h4>
+              <p className={styles.itemCard__pos}>Vice President</p>
+            </div>
+          </li>
+          <li className={styles.itemCard}>
+            <div className={styles.overlay}>
+              <picture>
+                <source
+                  type="image/webp"
+                  media="(min-width: 1360px)"
+                  srcSet={`${thirdImages.deskWebp3} 1x, ${thirdImages.deskWebp2x3} 2x`}
+                />
+                <source
+                  type="image/webp"
+                  media="(min-width: 768px)"
+                  srcSet={`${thirdImages.tabWebp3} 1x, ${thirdImages.tabWebp2x3} 2x`}
+                />
+                <source
+                  type="image/webp"
+                  media="(max-width: 767px)"
+                  srcSet={`${thirdImages.mobWebp3} 1x, ${thirdImages.mobWebp2x3} 2x`}
+                />
+                <source
+                  media="(min-width: 1360px)"
+                  srcSet={`${thirdImages.deskJpeg3} 1x, ${thirdImages.deskJpeg2x3} 2x`}
+                />
+                <source
+                  media="(min-width: 768px)"
+                  srcSet={`${thirdImages.tabJpeg3} 1x, ${thirdImages.tabJpeg2x3} 2x`}
+                />
+                <source
+                  media="(max-width: 767px)"
+                  srcSet={`${firstImages.mobJpeg3} 1x, ${thirdImages.mobJpeg2x3} 2x`}
+                />
+                <img
+                  className={styles._img}
+                  src={thirdImages.mobJpeg3}
+                  alt="about"
+                  width="680"
+                />
+              </picture>
+              {/*<div className={styles.itemCardOverlay}>*/}
+              {/*  <ul className={styles.social__list}>*/}
+              {/*    <li className={styles.social__item}>*/}
+              {/*      <a*/}
+              {/*        href="https://www.facebook.com/"*/}
+              {/*        target="_blank"*/}
+              {/*        rel="noopener noreferrer"*/}
+              {/*      >*/}
+              {/*        <svg*/}
+              {/*          className={styles.social__item_icon}*/}
+              {/*          width={35}*/}
+              {/*          height={35}*/}
+              {/*        >*/}
+              {/*          <use href={`${icon}#facebook`}></use>*/}
+              {/*        </svg>*/}
+              {/*      </a>*/}
+              {/*    </li>*/}
+              {/*    <li className={styles.social__item}>*/}
+              {/*      <a*/}
+              {/*        href="https://twitter.com/"*/}
+              {/*        target="_blank"*/}
+              {/*        rel="noopener noreferrer"*/}
+              {/*      >*/}
+              {/*        <svg*/}
+              {/*          className={styles.social__item_icon}*/}
+              {/*          width={35}*/}
+              {/*          height={33}*/}
+              {/*        >*/}
+              {/*          <use href={`${icon}#twitter`}></use>*/}
+              {/*        </svg>*/}
+              {/*      </a>*/}
+              {/*    </li>*/}
+              {/*    <li className={styles.social__item}>*/}
+              {/*      <a*/}
+              {/*        href="https://www.youtube.com/"*/}
+              {/*        target="_blank"*/}
+              {/*        rel="noopener noreferrer"*/}
+              {/*      >*/}
+              {/*        <svg*/}
+              {/*          className={styles.social__item_icon}*/}
+              {/*          width={40}*/}
+              {/*          height={31}*/}
+              {/*        >*/}
+              {/*          <use href={`${icon}#youtube`}></use>*/}
+              {/*        </svg>*/}
+              {/*      </a>*/}
+              {/*    </li>*/}
+              {/*    <li className={styles.social__item}>*/}
+              {/*      <a*/}
+              {/*        href="https://www.linkedin.com/"*/}
+              {/*        target="_blank"*/}
+              {/*        rel="noopener noreferrer"*/}
+              {/*      >*/}
+              {/*        <svg*/}
+              {/*          className={styles.social__item_icon}*/}
+              {/*          style={{ width: "35px", height: "39px" }}*/}
+              {/*        >*/}
+              {/*          <use href={`${icon}#linkedin`}></use>*/}
+              {/*        </svg>*/}
+              {/*      </a>*/}
+              {/*    </li>*/}
+              {/*  </ul>*/}
+              {/*</div>*/}
+            </div>
+            <div className={styles.itemCard__content}>
+              <h4 className={styles.itemCard__name}>Steve Smith</h4>
+              <p className={styles.itemCard__pos}>Marketing Head</p>
+            </div>
+          </li>
+        </ul>
       </Container>
     </section>
   );
---
+++ src/views/Team/images.js
@@ -0,0 +1,87 @@
+import mobWebp1 from '../../images/team/mobile/team1.webp';
+import mobWebp2x1 from '../../images/team/mobile/team1@2x.webp';
+import mobJpeg1 from '../../images/team/mobile/team1.jpg';
+import mobJpeg2x1 from '../../images/team/mobile/team1@2x.jpg';
+import mobWebp2 from '../../images/team/mobile/team2.webp';
+import mobWebp2x2 from '../../images/team/mobile/team2@2x.webp';
+import mobJpeg2 from '../../images/team/mobile/team2.jpg';
+import mobJpeg2x2 from '../../images/team/mobile/team2@2x.jpg';
+import mobWebp3 from '../../images/team/mobile/team3.webp';
+import mobWebp2x3 from '../../images/team/mobile/team3@2x.webp';
+import mobJpeg3 from '../../images/team/mobile/team3.jpg';
+import mobJpeg2x3 from '../../images/team/mobile/team3@2x.jpg';
+
+import tabWebp1 from '../../images/team/tablet/team1.webp';
+import tabWebp2x1 from '../../images/team/tablet/team1@2x.webp';
+import tabJpeg1 from '../../images/team/tablet/team1.jpg';
+import tabJpeg2x1 from '../../images/team/tablet/team1@2x.jpg';
+import tabWebp2 from '../../images/team/tablet/team2.webp';
+import tabWebp2x2 from '../../images/team/tablet/team2@2x.webp';
+import tabJpeg2 from '../../images/team/tablet/team2.jpg';
+import tabJpeg2x2 from '../../images/team/tablet/team2@2x.jpg';
+import tabWebp3 from '../../images/team/tablet/team3.webp';
+import tabWebp2x3 from '../../images/team/tablet/team3@2x.webp';
+import tabJpeg3 from '../../images/team/tablet/team3.jpg';
+import tabJpeg2x3 from '../../images/team/tablet/team3@2x.jpg';
+
+import deskWebp1 from '../../images/team/desktop/team1.webp';
+import deskWebp2x1 from '../../images/team/desktop/team1@2x.webp';
+import deskJpeg1 from '../../images/team/desktop/team1.jpg';
+import deskJpeg2x1 from '../../images/team/desktop/team1@2x.jpg';
+import deskWebp2 from '../../images/team/desktop/team2.webp';
+import deskWebp2x2 from '../../images/team/desktop/team2@2x.webp';
+import deskJpeg2 from '../../images/team/desktop/team2.jpg';
+import deskJpeg2x2 from '../../images/team/desktop/team2@2x.jpg';
+import deskWebp3 from '../../images/team/desktop/team3.webp';
+import deskWebp2x3 from '../../images/team/desktop/team3@2x.webp';
+import deskJpeg3 from '../../images/team/desktop/team3.jpg';
+import deskJpeg2x3 from '../../images/team/desktop/team3@2x.jpg';
+
+const firstImages = {
+  mobWebp1,
+  mobWebp2x1,
+  mobJpeg1,
+  mobJpeg2x1,
+  tabWebp1,
+  tabWebp2x1,
+  tabJpeg1,
+  tabJpeg2x1,
+  deskWebp1,
+  deskWebp2x1,
+  deskJpeg1,
+  deskJpeg2x1,
+};
+const secondImages = {
+  mobWebp2,
+  mobWebp2x2,
+  mobJpeg2,
+  mobJpeg2x2,
+  tabWebp2,
+  tabWebp2x2,
+  tabJpeg2,
+  tabJpeg2x2,
+  deskWebp2,
+  deskWebp2x2,
+  deskJpeg2,
+  deskJpeg2x2,
+};
+const thirdImages = {
+  mobWebp3,
+  mobWebp2x3,
+  mobJpeg3,
+  mobJpeg2x3,
+  tabWebp3,
+  tabWebp2x3,
+  tabJpeg3,
+  tabJpeg2x3,
+  deskWebp3,
+  deskWebp2x3,
+  deskJpeg3,
+  deskJpeg2x3,
+};
+
+export const images = {
+  firstImages,
+  secondImages,
+  thirdImages,
+};
